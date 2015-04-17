36bafe447c680db2ac7beeddc6f3d96c4e4332bf
Irvan <dolphinigle.mailbox@gmail.com>
2015-04-17 17:25:11 +0800

    Doge main solution, example, 2 big toy test cases.
    Part of #21

---
+++ doge/doge_dolphinigle.cc
@@ -0,0 +1,141 @@
+// Authored by dolphinigle
+
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+
+#include <queue>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <cstring>
+#include <cassert>
+
+#define FORN(X,Y) for (int (X) = 0;(X) < (Y);++(X))
+#define REP(X,Y,Z) for (int (X) = (Y);(X) < (Z);++(X))
+
+#define SZ(Z) ((int)(Z).size())
+#define ALL(W) (W).begin(), (W).end()
+#define PB push_back
+
+#define MP make_pair
+#define A first
+#define B second
+
+#define INF 1023123123
+#define EPS 1e-11
+
+#define MX(Z,Y) Z = max((Z),(Y))
+#define MN(X,Y) X = min((X),(Y))
+
+using namespace std;
+
+typedef long long ll;
+typedef double db;
+typedef vector<int> vint;
+
+const int kMaxN = 100005;
+const int kMaxAnswer = 10000005;
+const int kMaxSqrtN = (int)sqrt(kMaxN) + 10;
+
+vector<int> powers[kMaxN];  // List of doges in a building.
+stack<ll> q[kMaxAnswer];
+int done[kMaxN][kMaxSqrtN];
+
+int n, m;
+ll Encode(int building, int power) {
+  assert(power <= kMaxSqrtN);
+  return building + power * n;
+}
+
+#ifdef DOLPHINIGLE_ENV
+int main_a() {
+#else
+int main() {
+#endif
+  cin >> n >> m;
+  int initial = -1;
+  FORN(i, m) {
+    int building, power;
+    cin >> building >> power;
+    powers[building].push_back(power);
+    if (!i) initial = building;
+  }
+  int max_m = (int)sqrt(n);
+  // The graph has two kinds of nodes:
+  // 1) (building, power) node				printf("%02hX goes in %hu to %02hX\n", (unsigned short int)a, (unsigned short int)d, (unsigned short int)other);
+  // 2) (building) node
+  // We represent the second type with (building, 0).
+  q[0].push(1LL * initial);
+
+  int current_answer = 0;
+  int best_answer = -1;
+  while (current_answer < kMaxAnswer) {
+    if (q[current_answer].empty()) {
+      ++current_answer;
+      continue;
+    }
+    ll top = q[current_answer].top();
+    q[current_answer].pop();
+    int building = top % n;
+    int power = top / n;
+    if (done[building][power]) continue;
+    done[building][power] = true;
+    if (building == 1) {
+      if (best_answer == -1) {
+        best_answer = current_answer;
+      }
+      continue;
+    }
+    if (!power) {
+      // in a building. Transition to every doges in the building that are weak, or make a jump.
+      for (int doge : powers[building]) {
+        if (doge <= max_m) {
+          // go to doge!
+          q[current_answer].push(Encode(building, doge));
+        } else {
+          // all possible locations: forward and backwards.
+          for (int dir = -1; dir < 2; dir += 2) {
+            for (int jumps = 1;;++jumps) {
+              int pos = building + doge * dir * jumps;
+              if (pos < 0 || pos >= n) break;
+              q[current_answer + jumps].push(Encode(pos, 0));
+            }
+          }
+        }
+      }
+    } else {
+      // have a power.
+      // Transition to building.
+      q[current_answer].push(Encode(building, 0));
+      // Jump front and back.
+      int pos = building + power;
+      if (pos < n) q[current_answer + 1].push(Encode(pos, power));
+      pos = building - power;
+      if (pos >= 0) q[current_answer + 1].push(Encode(pos, power));
+    }
+  }
+  cout << best_answer << endl;
+  return 0;
+}
+
+
+
+
+
+
+
+
+
---
+++ doge/gen2.py
@@ -0,0 +1,10 @@
+import random
+print 100000, 100000
+print 99998, 2
+print 1, 1
+print 0, 3
+print 99999, 1
+for i in range(99996):
+  print random.randint(0, 100000-1), random.randint(50001, 100000)
+
+
