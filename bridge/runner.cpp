26efb04b4120d25ad5855f4c33c62cc42bab4504
Jonathan Irvin Gunawan <jonathan.irvin@yahoo.com>
2015-05-07 11:15:00 +0700

    Stronger testcases for bridge. Handles special cases of intervals.
    Part of #24

--- bridge/runner.cpp
+++ bridge/runner.cpp
@@ -123,17 +123,42 @@ protected:
         CASE(K = 1, N = 1000, makeArray(1000000));
         CASE(K = 1, N = 967, makeArray(1000000000));
         CASE(K = 1, N = 1000, makeArray(1000000000));
+        CASE(K = 1, N = randomIntBetween(1,1000), makeArray(1));
+        CASE(K = 1, N = randomIntBetween(1,1000), makeArray(100));
+        CASE(K = 1, N = randomIntBetween(1,1000), makeArray(1000000));
+
+        CASE(K = 1, N =  randomIntBetween(1,1000), allCrossingFullInterval(1000000));
+        CASE(K = 1, N =  randomIntBetween(1,1000), allCrossingNoIntersect(1000000));
+        CASE(K = 1, N =  randomIntBetween(1,1000), allCrossingAllOverlap(1000000));
+        CASE(K = 1, N =  randomIntBetween(1,1000), allCrossingSmallInterval(1000000));
+        CASE(K = 1, N =  1000, allCrossingFullInterval(1000000000));
+        CASE(K = 1, N =  1000, allCrossingNoIntersect(1000000000));
+        CASE(K = 1, N =  1000, allCrossingAllOverlap(1000000000));
+        CASE(K = 1, N =  1000, allCrossingSmallInterval(1000000000));
     }
 
     void TestGroup2() {
         //K = 1
-        //1 <= N <= 100000
+        //1001 <= N <= 100000
         assignToSubtasks({2});
 
         CASE(K = 1, N =  100000, makeArray(1));
         CASE(K = 1, N =  100000, makeArray(100));
         CASE(K = 1, N =  100000, makeArray(1000000));
         CASE(K = 1, N =  100000, makeArray(1000000000));
+        CASE(K = 1, N =  randomIntBetween(1001,100000), makeArray(1));
+        CASE(K = 1, N =  randomIntBetween(1001,100000), makeArray(100));
+        CASE(K = 1, N =  randomIntBetween(1001,100000), makeArray(1000000));
+        CASE(K = 1, N =  randomIntBetween(1001,100000), makeArray(1000000000));
+
+        CASE(K = 1, N =  randomIntBetween(1001,100000), allCrossingFullInterval(1000000));
+        CASE(K = 1, N =  randomIntBetween(1001,100000), allCrossingNoIntersect(1000000));
+        CASE(K = 1, N =  randomIntBetween(1001,100000), allCrossingAllOverlap(1000000));
+        CASE(K = 1, N =  randomIntBetween(1001,100000), allCrossingSmallInterval(1000000));
+        CASE(K = 1, N =  100000, allCrossingFullInterval(1000000000));
+        CASE(K = 1, N =  100000, allCrossingNoIntersect(1000000000));
+        CASE(K = 1, N =  100000, allCrossingAllOverlap(1000000000));
+        CASE(K = 1, N =  100000, allCrossingSmallInterval(1000000000));
     }
 
     void TestGroup3() {
@@ -149,11 +174,26 @@ protected:
         CASE(K = 2, N =  87, makeArray(1000000000));
         CASE(K = 2, N =  100, makeArray(1000000000));
         CASE(K = 2, N =  100, makeArray(1000000000));
+        CASE(K = 2, N =  randomIntBetween(1,100), makeArray(1));
+        CASE(K = 2, N =  randomIntBetween(1,100), makeArray(100));
+        CASE(K = 2, N =  randomIntBetween(1,100), makeArray(1000000));
+        CASE(K = 2, N =  randomIntBetween(1,87), makeArray(1000000000));
+        CASE(K = 2, N =  randomIntBetween(1,100), makeArray(1000000000));
+        CASE(K = 2, N =  randomIntBetween(1,100), makeArray(1000000000));
+
+        CASE(K = 2, N =  randomIntBetween(1,100), allCrossingFullInterval(1000000));
+        CASE(K = 2, N =  randomIntBetween(1,100), allCrossingNoIntersect(1000000));
+        CASE(K = 2, N =  randomIntBetween(1,100), allCrossingAllOverlap(1000000));
+        CASE(K = 2, N =  randomIntBetween(1,100), allCrossingSmallInterval(1000000));
+        CASE(K = 2, N =  100, allCrossingFullInterval(1000000000));
+        CASE(K = 2, N =  100, allCrossingNoIntersect(1000000000));
+        CASE(K = 2, N =  100, allCrossingAllOverlap(1000000000));
+        CASE(K = 2, N =  100, allCrossingSmallInterval(1000000000));
     }
 
     void TestGroup4() {
         //K = 2
-        //1 <= N <= 1000
+        //101 <= N <= 1000
         assignToSubtasks({4, 5});
 
         CASE(K = 2, N =  1000, makeArray(1));
@@ -163,23 +203,54 @@ protected:
         CASE(K = 2, N =  993, makeArray(1000000000));
         CASE(K = 2, N =  958, makeArray(1000000000));
         CASE(K = 2, N =  1000, makeArray(1000000000));
+        CASE(K = 2, N =  randomIntBetween(101,1000), makeArray(1));
+        CASE(K = 2, N =  randomIntBetween(101,1000), makeArray(100));
+        CASE(K = 2, N =  randomIntBetween(101,1000), makeArray(10000));
+        CASE(K = 2, N =  randomIntBetween(101,1000), makeArray(1000000));
+
+        CASE(K = 2, N =  randomIntBetween(101,1000), allCrossingFullInterval(1000000));
+        CASE(K = 2, N =  randomIntBetween(101,1000), allCrossingNoIntersect(1000000));
+        CASE(K = 2, N =  randomIntBetween(101,1000), allCrossingAllOverlap(1000000));
+        CASE(K = 2, N =  randomIntBetween(101,1000), allCrossingSmallInterval(1000000));
+        CASE(K = 2, N =  1000, allCrossingFullInterval(1000000000));
+        CASE(K = 2, N =  1000, allCrossingNoIntersect(1000000000));
+        CASE(K = 2, N =  1000, allCrossingAllOverlap(1000000000));
+        CASE(K = 2, N =  1000, allCrossingSmallInterval(1000000000));
     }
 
     void TestGroup5() {
         //K = 2
-        //1 <= N <= 100000
+        //1001 <= N <= 100000
         assignToSubtasks({5});
 
         CASE(K = 2, N =  100000, makeArray(1));
         CASE(K = 2, N =  100000, makeArray(100));
         CASE(K = 2, N =  100000, makeArray(1000000));
         CASE(K = 2, N =  100000, makeArray(1000000000));
+        CASE(K = 2, N =  randomIntBetween(1001,100000), makeArray(1));
+        CASE(K = 2, N =  randomIntBetween(1001,100000), makeArray(100));
+        CASE(K = 2, N =  randomIntBetween(1001,100000), makeArray(1000000));
+        CASE(K = 2, N =  randomIntBetween(1001,100000), makeArray(1000000000));
+
+        CASE(K = 2, N =  randomIntBetween(1001,100000), allCrossingFullInterval(1000000000));
+        CASE(K = 2, N =  randomIntBetween(1001,100000), allCrossingNoIntersect(1000000000));
+        CASE(K = 2, N =  randomIntBetween(1001,100000), allCrossingAllOverlap(1000000000));
+        CASE(K = 2, N =  randomIntBetween(1001,100000), allCrossingSmallInterval(1000000000));
+        CASE(K = 2, N =  100000, allCrossingFullInterval(1000000000));
+        CASE(K = 2, N =  100000, allCrossingNoIntersect(1000000000));
+        CASE(K = 2, N =  100000, allCrossingAllOverlap(1000000000));
+        CASE(K = 2, N =  100000, allCrossingSmallInterval(1000000000));
     }
 
 private:
     mt19937 mt_rand; // Mersenne Twister pseudo-random generator
     int varA, varB, varN;
     double EPS = 1e-9;
+
+    int randomIntBetween(int minValue,int maxValue) {
+        uniform_int_distribution<int> dist(minValue, maxValue);
+        return dist(mt_rand);
+    }
 
     void makeArray(int mcd) {
         TS.clear(); TE.clear();
@@ -198,12 +269,68 @@ private:
         }
     }
 
-    int randomIntBetween(int minValue,int maxValue) {
-        uniform_int_distribution<int> dist(minValue, maxValue);
-        return dist(mt_rand);
+    void allCrossingFullInterval(int mcd) {
+        TS.clear(); TE.clear();
+        PS.clear(); PE.clear();
+        for (int i = 0; i < N; ++i) {
+            int fir = randomIntBetween(0,1), sec = 1 - fir;
+            int x = randomIntBetween(0,1) * mcd, y = mcd - x;
+            TS.push_back((char)(fir + 'A'));
+            TE.push_back((char)(sec + 'A'));
+            PS.push_back(x);
+            PE.push_back(y);
+        }
     }
 
+    void allCrossingNoIntersect(int mcd) {
+        TS.clear(); TE.clear();
+        PS.clear(); PE.clear();
+        int dif = mcd / N;
+        for (int i = 0; i < N; ++i) {
+            int fir = randomIntBetween(0,1), sec = 1 - fir;
+            int x = randomIntBetween(dif * i,dif * (i + 1) - 1);
+            int y = randomIntBetween(dif * i,dif * (i + 1) - 1);
+            TS.push_back((char)(fir + 'A'));
+            TE.push_back((char)(sec + 'A'));
+            PS.push_back(x);
+            PE.push_back(y);
+        }
+    }
 
+    void allCrossingAllOverlap(int mcd) {
+        TS.clear(); TE.clear();
+        PS.clear(); PE.clear();
+        int maks = mcd;
+        for (int i = 0; i < N; ++i) {
+            int fir = randomIntBetween(0,1), sec = 1 - fir;
+            int x = i;
+            int y = maks--;
+            if (randomIntBetween(0,1)) {
+                swap(x,y);
+            }
+            TS.push_back((char)(fir + 'A'));
+            TE.push_back((char)(sec + 'A'));
+            PS.push_back(x);
+            PE.push_back(y);
+        }
+    }
+
+    void allCrossingSmallInterval(int mcd) {
+        TS.clear(); TE.clear();
+        PS.clear(); PE.clear();
+        for (int i = 0; i < N; ++i) {
+            int fir = randomIntBetween(0,1), sec = 1 - fir;
+            int x = randomIntBetween(0,mcd-10);
+            int y = x + randomIntBetween(0,10);
+            if (randomIntBetween(0,1)) {
+                swap(x,y);
+            }
+            TS.push_back((char)(fir + 'A'));
+            TE.push_back((char)(sec + 'A'));
+            PS.push_back(x);
+            PE.push_back(y);
+        }
+    }
 };
 
 int main(int argc, char* argv[]) {
